87c7a7cafa611231150e75c12a81da86435f712e
桑茂森(Mou-Sen, Sang) <lukesang@gmail.com>
2016-07-24 12:06:53 +0800

    Based on MQTT to listen broker device message

---
+++ mqqt.js
@@ -0,0 +1,61 @@
+
+var mqtt = require('mqtt'), url = require('url');
+var querystring = require('querystring'); 
+var request = require("request");
+var Today=  new Date();
+var smartxlab_content={
+  uri: "https://getsmartx.com:3000/api/1/login",
+  method: "POST",
+  form: {
+    username: "ntut",
+	password: "ntut"
+  }
+}
+
+
+var url = 'mqtt://getsmartx.com';
+
+var Password_token;
+
+request(smartxlab_content, function(error, response, body) {
+
+  obj = JSON.parse(body);
+  console.log(obj.res.token);
+  Password_token=obj.res.token;
+  var options = {
+		  port: '1883',
+		  username: 'ntut',
+		  password: Password_token
+		};
+		
+var client = mqtt.connect(url,options);
+
+
+
+client.on('connect', function () {
+  client.subscribe('ntut/186',function(){
+		client.on('message', function (topic, message) {
+	  // message is Buffer 
+	  console.log(message.toString());
+	  client.end();
+	});
+  });
+});
+
+client.on('error', function (error) {
+  console.log("%s",error);
+  //console.log("%s",options.password);
+  
+});
+
+client.on('packetreceive', function (packet) {
+  console.log("%s",packet);
+  //console.log("%s",options.password);
+  
+});
+});
+
+
+
+
+
